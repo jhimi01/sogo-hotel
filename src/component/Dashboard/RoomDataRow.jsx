3cfa92f89c12240b445c013d04b29cc2be17d595
Jhimi Talukder <talukderjhimi@gmail.com>
2023-06-24 21:30:38 +0600

    update modal

--- src/component/Dashboard/RoomDataRow.jsx
+++ src/component/Dashboard/RoomDataRow.jsx
@@ -3,9 +3,11 @@ import { useState } from 'react'
 import DeleteModal from '../Modal/DeleteModal'
 import { deleteRoom } from '../../api/rooms'
 import { toast } from 'react-hot-toast'
+import UpdateModal from '../Modal/UpdateModal'
 
 const RoomDataRow = ({ room, fetchRooms }) => {
     let [isOpen, setIsOpen] = useState(false)
+    let [isEditModal, setIsEditModal] = useState(false)
     function closeModal() {
         setIsOpen(false)
     }
@@ -66,14 +68,17 @@ const RoomDataRow = ({ room, fetchRooms }) => {
         modalHandler={modalHandler}
         id={room._id}></DeleteModal>
       </td>
+
+      {/* update room info modal */}
       <td className='px-5 py-5 border-b border-gray-200 bg-white text-sm'>
-        <span className='relative cursor-pointer inline-block px-3 py-1 font-semibold text-green-900 leading-tight'>
+        <span onClick={()=>setIsEditModal(true)} className='relative cursor-pointer inline-block px-3 py-1 font-semibold text-green-900 leading-tight'>
           <span
             aria-hidden='true'
             className='absolute inset-0 bg-green-200 opacity-50 rounded-full'
           ></span>
           <span className='relative'>Update</span>
         </span>
+        <UpdateModal isOpen={isEditModal} setIsEditModal={setIsEditModal}></UpdateModal>
       </td>
     </tr>
   )
